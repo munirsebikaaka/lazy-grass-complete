e896ca4681a59176e356fedf3b6908ec88fed340
munir sebikaaka <munirsebikaaka@gmail.com>
2024-06-25 11:02:23 +0300

    feat:positioned the navigation in a fixed place

--- script.js
+++ script.js
@@ -1,8 +1,56 @@
 "use strict";
 const clientsSlide = document.querySelectorAll(".slide-client");
+const prosDisc = document.querySelector(".products-discription");
+const workWithSections = document.querySelectorAll(".work-with");
+const heroSection = document.querySelector(".hero-section");
+const navigationCell = document.querySelector(".application-nav");
 const btnLeft = document.querySelector(".left");
 const btnRight = document.querySelector(".right");
 
+//POSITIONING THE NAVIGATION
+const navPosition = new IntersectionObserver(
+  function (entries) {
+    const [entry] = entries;
+    if (entry.isIntersecting) {
+      navigationCell.classList.add("fixed-nav");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+  }
+);
+navPosition.observe(heroSection);
+
+//SECTIONS TRANSITIONS
+workWithSections.forEach((el) => el.classList.add("pro-dis-hide"));
+const removingFunction = function (entries) {
+  const [entry] = entries;
+  if (entry.isIntersecting) {
+    workWithSections.forEach((el) => el.classList.remove("pro-dis-hide"));
+  }
+};
+const removeProClass = new IntersectionObserver(removingFunction, {
+  root: null,
+  threshold: 0,
+});
+workWithSections.forEach((el) => removeProClass.observe(el));
+//PRODUCTS HEAD
+prosDisc.classList.add("pro-dis-hide2");
+const obsFUNC = function (entries) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      prosDisc.classList.remove("pro-dis-hide2");
+    }
+  });
+};
+const prosObj = {
+  root: null,
+  threshold: 0,
+};
+
+const elPRODis = new IntersectionObserver(obsFUNC, prosDisc);
+elPRODis.observe(prosDisc);
 //CLIENT SLIDER FUNCTIONALLITY
 let curValue = 0;
 let maxlength = clientsSlide.length;
@@ -31,35 +79,19 @@ function slideToLeft() {
   slideSolution(curValue);
 }
 
-const autoSlide = function () {
-  setInterval(function () {
-    time--;
-    secTime--;
-    thirTime--;
-    if (time === 0) {
-      slideToRight();
-      console.log("its time");
-    }
-    if (secTime === 0) {
-      slideToRight();
-    }
-    if (thirTime === 0) {
-      slideToRight();
-      autoSlide();
-    }
-  }, 1000);
-};
-autoSlide();
-let reTime = 60;
-function tryReverse() {
-  setInterval(function () {
-    reTime--;
-    if (thirTime === 0) {
-      autoSlide();
-      console.log("its the last time");
-    }
-  }, 1000);
-}
-tryReverse();
+setInterval(function () {
+  time--;
+  secTime--;
+  thirTime--;
+  if (time === 0) {
+    slideToRight();
+  }
+  if (secTime === 0) {
+    slideToRight();
+  }
+  if (thirTime === 0) {
+    slideToRight();
+  }
+}, 1000);
 btnRight.addEventListener("click", slideToRight);
 btnLeft.addEventListener("click", slideToLeft);
